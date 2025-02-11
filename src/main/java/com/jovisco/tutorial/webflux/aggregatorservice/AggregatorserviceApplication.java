ecc438b2287804d9dc31e6d78c7a5deec1c6b39a
Josef Heiss <josef.heiss@jovisco.de>
2025-02-11 16:05:37 +0100

    initial

---
+++ src/main/java/com/jovisco/tutorial/webflux/aggregatorservice/AggregatorserviceApplication.java
@@ -0,0 +1,13 @@
+package com.jovisco.tutorial.webflux.aggregatorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AggregatorserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AggregatorserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jovisco/tutorial/webflux/aggregatorservice/AggregatorserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jovisco.tutorial.webflux.aggregatorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AggregatorserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
